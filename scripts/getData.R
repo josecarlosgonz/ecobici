c7f18aa4aa6d793f43d4f762b841a94aaf23dd89
Jose Gonzalez <josecarlosgonz@gmail.com>
2013-08-19 23:40:30 -0400

    Get data

---
+++ scripts/getData.R
@@ -0,0 +1,15 @@
+### Import data
+## http://api.citybik.es/
+library(RJSONIO)
+
+help(RJSONIO)
+
+data  <- fromJSON("http://api.citybik.es/ecobici.json")
+
+str(data) #Explore a bit
+varNames <- names(data[[1]]) #get var names
+
+data  <- do.call(rbind.data.frame, data) #Unlist data
+names(data)  <- varNames
+
+head(data)
